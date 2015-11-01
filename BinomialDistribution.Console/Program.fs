89426ef2feffe6e047fd4bc288bdfa6be60db29d
Konstantin Finagin <konstantin.finagin@dataart.com>
2015-11-01 13:42:47 +0300

    initial commit, chapter 02 in work

---
+++ BinomialDistribution.Console/Program.fs
@@ -0,0 +1,59 @@
+﻿module BinomialDistribution.Program
+
+let Binomial x n p = 
+    let f = float
+    let cumulative = true
+    let mutable totalUnscaledP = 0.
+    let mutable unscaledResult = 0.
+    let essentialyZero = 10.E-12
+
+    let m = (f(n) * p) |> truncate |> int
+
+    totalUnscaledP <- totalUnscaledP + 1.
+
+    if m = x then unscaledResult <- unscaledResult + 1.
+    if cumulative && m < x then unscaledResult <- unscaledResult + 1.
+    let mutable previousValue = 1.
+    let mutable isDone = false
+
+    let mutable k = m + 1
+
+    while not isDone && k <= n do
+        let currentValue = previousValue * f(n - k + 1) * p / (f(k) * (1. - p))
+        totalUnscaledP <- totalUnscaledP + currentValue
+        
+        if k = x then unscaledResult <- unscaledResult + currentValue
+        if cumulative && k < x then unscaledResult <- unscaledResult + currentValue
+        if currentValue <= essentialyZero then isDone <- true
+        previousValue <- currentValue
+        k <- k + 1
+
+    previousValue <- 1.
+    isDone <- false
+    k <- m - 1
+
+    while not isDone && k >= 0 do
+        let currentValue = previousValue * f(k + 1) * (1. - p) / (f(n - k) * p)
+        totalUnscaledP <- totalUnscaledP + currentValue
+        
+        if k = x then unscaledResult <- unscaledResult + currentValue
+        if cumulative && k < x then unscaledResult <- unscaledResult + currentValue
+
+        if currentValue <= essentialyZero then isDone <- true
+        previousValue <- currentValue
+        k <- k - 1
+
+    unscaledResult / totalUnscaledP
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ BinomialDistribution.Tests/Tests.fs
@@ -0,0 +1,19 @@
+﻿namespace BinomialDistribution.Tests
+
+open FsUnit 
+open NUnit.Framework
+open BinomialDistribution.Program
+
+[<TestFixture>]
+type ``Given the Binomial Function``() =
+
+    [<TestCase(1,10,0.5,0.0107421875)>]
+    [<TestCase(2,10,0.5,0.0546875)>]
+    [<TestCase(3,10,0.5,0.171875)>]
+    [<TestCase(4,10,0.5,0.376953125)>]
+    [<TestCase(5,10,0.5,0.623046875)>]
+    [<TestCase(5,13,0.5,0.29052734375)>]
+    member t.``the result is calculated correctly``(x, n, p, expected) =
+        let errorMargin = 1e-9
+        let actual = Binomial x n p
+        actual |> should (equalWithin errorMargin) expected
