e8620d4ed1b6864ca7f2b7d48f4d6c02edb03052
Konstantin Finagin <konstantin.finagin@dataart.com>
2015-11-01 15:38:33 +0300

    chapter02, rewriting algorithm with F#

--- BinomialDistribution.Console/Program.fs
+++ BinomialDistribution.Console/Program.fs
@@ -2,7 +2,6 @@
 
 let Binomial x n p = 
     let f = float
-    let cumulative = true
     let mutable totalUnscaledP = 0.
     let mutable unscaledResult = 0.
     let essentialyZero = 10.E-12
@@ -12,36 +11,47 @@ let Binomial x n p =
     totalUnscaledP <- totalUnscaledP + 1.
 
     if m = x then unscaledResult <- unscaledResult + 1.
-    if cumulative && m < x then unscaledResult <- unscaledResult + 1.
+
+
+    let CalcCurrent value k = 
+        if k > m then 
+            value * float(n - k + 1) * p / (float(k) * (1. - p))
+        else
+            value * float(k + 1) * (1. - p) / (float(n - k) * p)
+
+    let CalcUnscaled x k acc increment =
+        if k <= x  then acc + increment
+        else acc
+
+    let Done current = current <= essentialyZero
+
+    let NextK k = if k > m then k + 1 else k - 1
+
+    
+
     let mutable previousValue = 1.
     let mutable isDone = false
-
     let mutable k = m + 1
 
     while not isDone && k <= n do
-        let currentValue = previousValue * f(n - k + 1) * p / (f(k) * (1. - p))
-        totalUnscaledP <- totalUnscaledP + currentValue
-        
-        if k = x then unscaledResult <- unscaledResult + currentValue
-        if cumulative && k < x then unscaledResult <- unscaledResult + currentValue
-        if currentValue <= essentialyZero then isDone <- true
+        let currentValue = CalcCurrent previousValue k
+        totalUnscaledP <- totalUnscaledP + currentValue        
+        unscaledResult <- CalcUnscaled x k unscaledResult currentValue
+        isDone <- Done currentValue
         previousValue <- currentValue
-        k <- k + 1
+        k <- NextK k
 
     previousValue <- 1.
     isDone <- false
     k <- m - 1
 
     while not isDone && k >= 0 do
-        let currentValue = previousValue * f(k + 1) * (1. - p) / (f(n - k) * p)
+        let currentValue = CalcCurrent previousValue k
         totalUnscaledP <- totalUnscaledP + currentValue
-        
-        if k = x then unscaledResult <- unscaledResult + currentValue
-        if cumulative && k < x then unscaledResult <- unscaledResult + currentValue
-
-        if currentValue <= essentialyZero then isDone <- true
+        unscaledResult <- CalcUnscaled x k unscaledResult currentValue
+        isDone <- Done currentValue
         previousValue <- currentValue
-        k <- k - 1
+        k <- NextK k
 
     unscaledResult / totalUnscaledP
 
