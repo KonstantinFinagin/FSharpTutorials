a2c73e176dd89711ab3121824758f42ed754d9f4
Konstantin Finagin <konstantin.finagin@dataart.com>
2015-11-02 23:50:02 +0300

    added first simple parsing function

---
+++ Markdown.Console/Parser.fs
@@ -0,0 +1,15 @@
+﻿module Parser
+
+open Types
+
+let rec parseInlineBody acc = function
+    | '`'::rest -> 
+        Some(List.rev acc, rest)
+    | c::chars ->
+        parseInlineBody (c::acc) chars
+    | [] -> None
+
+let parseInline = function
+    | '`'::chars ->
+        parseInlineBody [] chars
+    | _ -> None
---
+++ Markdown.Console/Types.fs
@@ -0,0 +1,17 @@
+﻿module Types
+
+type MarkdownDocument = list<MarkdownBlock>
+
+and MarkdownBlock =
+    | Heading of int * MarkdownSpans
+    | Paragraph of MarkdownSpans
+    | CodeBlock of list<string>
+
+and MarkdownSpans = list<MarkdownSpan>
+
+and MarkdownSpan = 
+    | Literal of string
+    | InlineCode of string
+    | Strong of MarkdownSpans
+    | Emphasis of MarkdownSpans
+    | Hyperlink of MarkdownSpans * string
