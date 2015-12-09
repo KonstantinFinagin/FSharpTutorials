5af57b1b8a174fb34e66ed7564c2a8467f96c01b
Konstantin Finagin <konstantin.finagin@gmail.com>
2015-12-09 23:35:50 +0300

    -

--- Markdown.Console/Program.fs
+++ Markdown.Console/Program.fs
@@ -1,2 +1,8 @@
 ﻿open Parser
+open System
 
+[<EntryPoint>]
+let main args =
+    let result ="hello world! [http://lenta.ru](Lenta hyperlink)" |> List.ofSeq |> parseSpans [] |> List.ofSeq
+    let rln = Console.ReadLine()
+    0
