f8adb56f3c35d8e311e45fed64f0dd13761d573c
Konstantin Finagin <konstantin.finagin@dataart.com>
2016-02-05 20:20:40 +0300

    -

--- SocialNetworks.Console/Program.fs
+++ SocialNetworks.Console/Program.fs
@@ -1,7 +1,31 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open FSharp.Data.Toolbox.Twitter
+
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    
+    let key = "RZClDkP8rj06E0wB4G09rDX9b"
+    let secret = "vSzrFhDadmgFY6QdMfouBWMX5AWsGhkHFKSbG3qRBliB5RC0nf"
+
+    let twitter = Twitter.AuthenticateAppOnly(key, secret)
+
+    let friends = twitter.Connections.FriendsIds(screenName="@fsharporg")
+    let followers = twitter.Connections.FollowerIds(screenName="@fsharporg")
+
+    let idsOfInterest = Seq.append friends.Ids followers.Ids |> set
+
+    let groupedIds =
+        idsOfInterest
+        |> Seq.mapi (fun i id -> (i/100, id))
+        |> Seq.groupBy fst
+
+    let twitterNodes = [| for _, group in groupedIds do
+                            let ids = Seq.map snd group
+                            let nodeInfo = 
+                                twitter.Users.Lookup(ids)
+                                |> Array.map (fun node -> node.Id, node.ScreenName)
+                            yield! nodeInfo |]
+
+    System.Windows.Forms.Application.Run()
+
     0 // return an integer exit code
