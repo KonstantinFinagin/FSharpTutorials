968acdb335be6bb8285c4a299f3f23bb9a67b3f5
Konstantin Finagin <konstantin.finagin@dataart.com>
2016-02-15 20:27:30 +0300

    added playground

---
+++ Playground.Recursion/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+
+let map converter l = 
+    let rec loop accumulator = function
+        | [] -> accumulator
+        | head :: tail -> loop(converter head :: accumulator) tail 
+    List.rev (loop [] l)
+
+
+let a = map (fun x -> x * x) [1;2;3;4;5]
+
+let result =Console.ReadLine()
+0
