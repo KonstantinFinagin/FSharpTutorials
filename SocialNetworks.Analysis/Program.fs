13ef8b82a543e4f099eb3fcfa2e831c197cf7162
Konstantin Finagin <konstantin.finagin@dataart.com>
2016-02-18 16:58:20 +0300

    data added and new project for analysis is created

---
+++ SocialNetworks.Analysis/Program.fs
@@ -0,0 +1,10 @@
+﻿open FSharp.Data
+
+type Users = JsonProvider<"fsharporgNodes.json">
+let userNames = Users.Load("fsharporgNodes.json")
+
+type Connections = JsonProvider<"fsharporgLinks.json">
+let userLinks = Connections.Load("fsharporgLinks.json")
+
+
+0
